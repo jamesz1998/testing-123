5e197ab70d33fd786db126caf2c7a8c99f8ef8ad
jamesz1998 <jameszungunde@gmail.com>
2020-09-21 18:53:19 +0200

    Testing

---
+++ git-test-project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git-test-project
+//
+//  Created by James Zungunde on 2020/09/21.
+//  Copyright © 2020 James Zungunde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
