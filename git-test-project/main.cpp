8b5744d1d85293471ff30afaffbf89aa0badf395
jamesz1998 <jameszungunde@gmail.com>
2020-09-21 19:13:04 +0200

    changes baby

--- git-test-project/main.cpp
+++ git-test-project/main.cpp
@@ -8,8 +8,10 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    
+    cout<<" this is a test"<<endl;
     return 0;
 }
